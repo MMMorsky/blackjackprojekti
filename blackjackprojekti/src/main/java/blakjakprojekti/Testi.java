f742e7d7ee6e15eccd6c9c61cceb46b005a0a680
max <max.morsky@gmail.com>
2017-01-20 00:49:05 +0200

    projektiluotu

---
+++ blackjackprojekti/src/main/java/blakjakprojekti/Testi.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blakjakprojekti;
+
+/**
+ *
+ * @author max
+ */
+public class Testi {
+    private int arvo;
+
+    public Testi() {
+        this.arvo = 3;
+    }
+
+    public int getArvo() {
+        return arvo;
+    }
+    
+    
+    
+    
+}
---
+++ dokumentaatio/aiheenKuvausJaRakenne.md
@@ -0,0 +1,1 @@
+kaikenlaista :D
